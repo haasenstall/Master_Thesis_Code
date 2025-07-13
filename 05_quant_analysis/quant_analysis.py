a124362445ca8ed5c19cb796d3c3a972fcf94db9
felix haas <haasenpowerstall@gmail.com>
2025-07-13 21:35:04 +0200

    structure -- adding files

---
+++ 05_quant_analysis/quant_analysis.py
@@ -0,0 +1,8 @@
+"""
+Master Thesis: Value Creation of Data Platforms by Facilitating Secondary Data - Quantitative Analysis of Clinical Trials
+
+The first step is analysisng meta data from nct ids and cluster them into different categories
+- The NCT ID is a unique identifier for clinical trials conducted in the United States and around the world
+- every NCT ID is linked to a unique reqeust ID from each dataplatform
+- every NCT ID has therefore an indicator of output
+"""
--- quant_analysis.py
+++
@@ -1,8 +0,0 @@
-"""
-Master Thesis: Value Creation of Data Platforms by Facilitating Secondary Data - Quantitative Analysis of Clinical Trials
-
-The first step is analysisng meta data from nct ids and cluster them into different categories
-- The NCT ID is a unique identifier for clinical trials conducted in the United States and around the world
-- every NCT ID is linked to a unique reqeust ID from each dataplatform
-- every NCT ID has therefore an indicator of output
-"""
